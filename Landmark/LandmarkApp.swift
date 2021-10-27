ba3b534b460bd981fe2726d6bbf2c840deb80d71
ichikawa7ss <i2s7.chacha@gmail.com>
2021-10-27 10:55:43 +0900

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by ichikawa on 2021/10/27.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
