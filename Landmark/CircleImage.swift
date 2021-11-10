24da698a116c61b6d6aba4b40eb39245a7af3b2d
ichikawa7ss <i2s7.chacha@gmail.com>
2021-11-10 13:19:09 +0900

    end Chapter1

---
+++ Landmark/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmark
+//
+//  Created by ichikawa on 2021/11/10.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .frame(width: 250, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
