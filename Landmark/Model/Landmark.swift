da51b9386480bff30098ed116a0916da57f4f387
ichikawa7ss <i2s7.chacha@gmail.com>
2021-11-17 11:16:27 +0900

    refactor and make model.

---
+++ Landmark/Model/Landmark.swift
@@ -0,0 +1,43 @@
+//
+//  Landmark.swift
+//  Landmark
+//
+//  Created by ichikawa on 2021/11/17.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    private var coordinates: Coordinates
+}
+
+// MARK: - computed property
+extension Landmark {
+
+    var image: Image {
+        Image(imageName)
+    }
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latifude,
+            longitude: coordinates.longitude
+        )
+    }
+}
+
+extension Landmark {
+    struct Coordinates: Hashable, Codable {
+        var latifude: Double
+        var longitude: Double
+    }
+}
