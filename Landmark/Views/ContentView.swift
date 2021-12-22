bdb2cae141a2d5d7fbe7042830569e19934d9b5d
ichikawa7ss <i2s7.chacha@gmail.com>
2021-12-22 12:19:42 +0900

    adopt the model object in views.

--- Landmark/Views/ContentView.swift
+++ Landmark/Views/ContentView.swift
@@ -9,22 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink(
-                    destination: LandmarkDetail(landmark: landmark),
-                    label: {
-                        LandmarkRow(landmark: landmark)
-                    }
-                )
-            }
-            .navigationTitle("Landmark")
-        }
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ModelData())
     }
 }
