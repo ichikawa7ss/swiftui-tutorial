aa8c3c10dfb0071c86afcf4092b2755f41f295da
ichikawa7ss <i2s7.chacha@gmail.com>
2022-01-10 00:46:15 +0900

    add profile.

---
+++ Landmark/Views/HikeBadge.swift
@@ -0,0 +1,31 @@
+//
+//  HikeBadge.swift
+//  Landmark
+//
+//  Created by ichikawa on 2022/01/10.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    let name: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name)")
+            
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
